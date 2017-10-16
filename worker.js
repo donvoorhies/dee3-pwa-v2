e1ca3ad7ccbd5506f96cbdaa297f25b4142bfc58
Don Voorhies <donvoorhies@gmail.com>
2017-10-16 21:17:54 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,20 @@
 /*http://deanhume.com/Home/BlogPost/create-a-really--really-simple-offline-page-using-service-workers/10135
 https://github.com/deanhume/Service-Worker-Offline*/
-var cacheVersion=1,currentCache={offline:"offline-cache"+cacheVersion},offlineUrl="offline.html";this.addEventListener("install",function(a){a.waitUntil(caches.open(currentCache.offline).then(function(a){return a.addAll([offlineUrl])}))});this.addEventListener("fetch",function(a){"navigate"===a.request.mode||"GET"===a.request.method&&a.request.headers.get("accept").includes("text/html")?a.respondWith(fetch(a.request.url)["catch"](function(a){return caches.match(offlineUrl)})):a.respondWith(caches.match(a.request).then(function(b){return b||fetch(a.request)}))});
+'use strict';
+
+var cacheVersion = 1;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'offline.html';
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+          'offline.html',
+          offlineUrl
+      ]);
+    })
+  );
+});
